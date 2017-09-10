8a0f604cb06312b0b461ccab2f76644abb7f07f8
Julio Soto <juliosotodev@gmail.com>
2017-09-10 01:49:47 -0600

    separated code and added classes.js

---
+++ classes.js
@@ -0,0 +1,26 @@
+// classes ans objects
+
+var champion = {
+  X : 150,
+  Y : 250,
+  speed : 5,
+  Xdir : -1,
+  Ydir : 0
+}
+var POS = function (x,y,size,speed,xs,ys) {
+  this.X = x;
+  this.Y = y;
+  this.size = size;
+  this.speed = speed;
+  this.Xs = xs;
+  this.Ys = ys;
+  this.move = function(){
+    this.X += this.Xs * this.speed;
+    this.Y += this.Ys * this.speed;
+    if(this.X > 850 || this.X < 0)
+      this.Xs = this.Xs * -1;
+     if(this.Y > 450 || this.Y < 0)
+      this.Ys = this.Ys * -1;
+  }
+}
+
--- main.js
+++ main.js
@@ -5,34 +5,7 @@ window.addEventListener('keydown',readKey,false);
 window.addEventListener('resize',resizeContainer,false);
 
 
-
-var POS = function (x,y,size,speed,xs,ys) {
-  this.X = x;
-  this.Y = y;
-  this.size = size;
-  this.speed = speed;
-  this.Xs = xs;
-  this.Ys = ys;
-  this.move = function(){
-    this.X += this.Xs * this.speed;
-    this.Y += this.Ys * this.speed;
-    if(this.X > 850 || this.X < 0)
-      this.Xs = this.Xs * -1;
-     if(this.Y > 450 || this.Y < 0)
-      this.Ys = this.Ys * -1;
-  }
-}
-
 var myPOS = new POS(800,100,20,5,-3,1);
-
-var champion = {
-  X : 150,
-  Y : 250,
-  speed : 5,
-  Xdir : -1,
-  Ydir : 0
-}
-
 
 function animation() {
   //champion.X -= champion.speed * champion.Xdir;
